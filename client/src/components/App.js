a9586b658a59ff0ea5c70576c31f36acb4524a68
Emmanuel Egomson <fegomson@gmail.com>
2022-08-22 15:22:15 -0500

    Setup navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from '../components';
+import { Home, SignIn, SignUp } from '../pages';
 
 const App = () => {
   return (
     <div className="App">
-     <h1>Welcome to the frontend</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/signin" element={<SignIn />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from '../pages/home/Home';
+export { default as SignUp } from '../pages/signup/SignUp';
+export { default as SignIn } from '../pages/signin/SignIn';
