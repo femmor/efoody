99b3fce724137c916ad0ab23b0ec7e47dca48f78
Emmanuel Egomson <fegomson@gmail.com>
2022-08-23 13:52:28 -0500

    Setup CORS, Morgan and Concurrently

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 import express from 'express';
-import dotenv from "dotenv"
-import { connectDB } from "./database/db.js"
-
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import { connectDB } from './database/db.js';
 
 const app = express();
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
+
+// middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello eFoody!')
-})
+  res.send('Hello eFoody!');
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
