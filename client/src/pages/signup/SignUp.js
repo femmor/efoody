481e8b7ea920e5929fdf0392035658a389905229
Emmanuel Egomson <fegomson@gmail.com>
2022-08-25 12:53:18 -0500

    Fixes creating new user login

--- client/src/pages/signup/SignUp.js
+++ client/src/pages/signup/SignUp.js
@@ -47,6 +47,7 @@ const SignUp = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
+
     setFormData({
       ...formData,
       loading: false,
@@ -108,7 +109,7 @@ const SignUp = () => {
     if (usernameRef.current) {
       usernameRef.current.focus();
     }
-  }, [usernameRef]);
+  }, []);
 
   const showSignUpForm = () => (
     <>
