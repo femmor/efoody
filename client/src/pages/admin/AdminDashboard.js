fcb36df6195e361f13bb6b52273b4c3e5dc35d28
Emmanuel Egomson <fegomson@gmail.com>
2022-08-26 17:18:31 -0500

    Adds action buttons in admin dashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,4 +1,5 @@
-import { MdDashboard } from 'react-icons/md';
+import { MdDashboard, MdCategory, MdOutlineFastfood } from 'react-icons/md';
+import { RiCurrencyFill } from 'react-icons/ri';
 
 const AdminDashboard = () => {
   // Views
@@ -17,6 +18,35 @@ const AdminDashboard = () => {
     </div>
   );
 
-  return <section className="">{showHeader()}</section>;
+  const showActionBtms = () => (
+    <div className="bg-light py-4">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 d-grid mb-2">
+            <button className="btn btn-outline-info d-flex align-items-center gap-1 justify-content-center">
+              <MdCategory /> Add Category
+            </button>
+          </div>
+          <div className="col-md-4 d-grid mb-2">
+            <button className="btn btn-outline-warning d-flex align-items-center gap-1 justify-content-center">
+              <MdOutlineFastfood /> Add Food
+            </button>
+          </div>
+          <div className="col-md-4 d-grid ">
+            <button className="btn btn-outline-success d-flex align-items-center gap-1 justify-content-center">
+              <RiCurrencyFill /> View Orders
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <section className="">
+      {showHeader()}
+      {showActionBtms()}
+    </section>
+  );
 };
 export default AdminDashboard;
